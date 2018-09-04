8e381be20c22e2779d1f9fb442db2e5db9328f30
Robert Fono <fonorobert@gmail.com>
2018-09-04 15:03:09 -0700

    break out reasons into separate js file

---
+++ reasons.js
@@ -0,0 +1,6 @@
+var reasons = [
+    "my dog got sick and now I've got to comfort her",
+    "i have a super important work project coming up in 3 months",
+    "i overate on falafel last night"
+]
+
