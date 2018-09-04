c7247a143e057ce9b2a373c193ec8c9799af5588
Robert Fono <fonorobert@gmail.com>
2018-09-04 16:37:46 -0700

    add some reasons

--- reasons.js
+++ reasons.js
@@ -1,6 +1,16 @@
 var reasons = [
     "my dog got sick and now I've got to comfort her",
     "i have a super important work project coming up in 3 months",
-    "i overate on falafel last night"
+    "i overate on falafel last night",
+	"my shoes got stolen and now i don't have any",
+	"the street in front of my house is flooding",
+	"there's a bear in front of my apartment",
+	"my mom's visiting and she doesn't let me stay out late",
+	"i've got an ant in the kitchen and i've gotta get rid of it",
+	"i'm waiting for an amazon package to arrive",
+	"my hairdresser said i can't go outside for a day",
+	"my doctor said the outside air is bad for me",
+	"i've got to help my brother set up the new TV",
+	"there are no ubers in my neighborhood at all"
 ]
 
